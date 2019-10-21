d11b5c15085f0f99b17ad998f1c8d1b6c2bcc479
mbosselut <duchmol7@hotmail.com>
2019-10-21 10:34:24 +0200

    Welcome to Codaisseur

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Codaisseur
         </a>
         <Button 
           name='ClickMe' 
